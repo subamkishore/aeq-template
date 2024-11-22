1bf1a3e1d38d4ed36d2b386014a00eaa1b7034de
kishore36 <kishore@aequalisys.com>
2024-11-22 12:30:10 +0530

    Reactive form interview questions added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,23 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   userList:any = [];
 
+  formConfiguration = [
+    { key: 'firstName', label: 'First Name' },
+    { key: 'lastName', label: 'Last Name' },
+    { key: 'phoneNumber', label: 'Phone Number' },
+    { key: 'email', label: 'Email' },
+    { key: 'address', label: 'Address' }
+  ]
+  
+  formValue = [
+    { key: 'firstName', value: 'John' },
+    { key: 'lastName', value: 'Wilmar' },
+    { key: 'phoneNumber', value: '+91 9345345433' },
+    { key: 'email', value: 'john@gmail.com' },
+    { key: 'address', value: 'Street No 21' }
+  ]
+  
+
   constructor(private http: HttpClient){
     this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((data)=>{
       this.userList = data;
